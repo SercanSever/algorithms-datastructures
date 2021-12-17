da8699ead7f010877c70893150770283e85f9a32
Sercan Sever <sercan.sever16@gmail.com>
2021-12-17 16:48:56 +0300

    :hammer:DoublyLinkedList AddFirst AddLast AddAfter

--- Algorithms.Console/Program.cs
+++ Algorithms.Console/Program.cs
@@ -16,38 +16,56 @@
 // });
 #endregion
 
-#region LinkedListResults
+#region SinglyLinkedListResults
 
-using Algorithms.Datastructures.LinkedLists;
+// using Algorithms.Datastructures.LinkedLists;
+// using Algorithms.Datastructures.LinkedLists.Doubly;
 
-var singlyLinkedList = new SinglyLinkedList<int>();
-singlyLinkedList.AddFirst(1);
-singlyLinkedList.AddFirst(2);
-singlyLinkedList.AddFirst(3);
-// 3 2 1 <-- result
+// var singlyLinkedList = new SinglyLinkedList<int>();
+// singlyLinkedList.AddFirst(1);
+// singlyLinkedList.AddFirst(2);
+// singlyLinkedList.AddFirst(3);
+// // 3 2 1 <-- result
 
-singlyLinkedList.AddLast(4);
-singlyLinkedList.AddLast(5);
-// 3 2 1 4 5 <-- result
+// singlyLinkedList.AddLast(4);
+// singlyLinkedList.AddLast(5);
+// // 3 2 1 4 5 <-- result
 
-singlyLinkedList.AddAfter(singlyLinkedList.Head.Next, 32);
-// 3 2 32 1 4 5 <-- result
+// singlyLinkedList.AddAfter(singlyLinkedList.Head.Next, 32);
+// // 3 2 32 1 4 5 <-- result
 
-singlyLinkedList.RemoveFirst();
-// 2 32 1 4 5 <-- result
-singlyLinkedList.RemoveLast();
-// 2 32 1 4 <-- result
-singlyLinkedList.Remove(1);
-// 2 32 4 <-- result
+// singlyLinkedList.RemoveFirst();
+// // 2 32 1 4 5 <-- result
+// singlyLinkedList.RemoveLast();
+// // 2 32 1 4 <-- result
+// singlyLinkedList.Remove(1);
+// // 2 32 4 <-- result
 
-foreach (var item in singlyLinkedList)
-{
-   System.Console.WriteLine(item);
-}
-
+// foreach (var item in singlyLinkedList)
+// {
+//    System.Console.WriteLine(item);
+// }
 
 #endregion
 
+#region DounlyLinkedListResults
+using Algorithms.Datastructures.LinkedLists.Doubly;
 
+var doublyList = new DoublyLinkedList<int>();
+doublyList.AddFirst(1);
+doublyList.AddFirst(2);
+// 2 1 <-- result
+
+doublyList.AddLast(3);
+doublyList.AddLast(4);
+// 2 1 3 4 <-- result
+
+doublyList.AddAfter(doublyList.Head.Next, new DoublyLinkedListNode<int>(5)
+);
+// 2 1 5 3 4 <-- result
+
+
+
+#endregion
 
 
